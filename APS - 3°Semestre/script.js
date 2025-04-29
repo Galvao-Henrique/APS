3bb10d4830137d4e4a9bb7cc58a3bfa5d1061912
Henrique Galvão <henriquepires114@gmail.com>
2025-04-29 20:37:55 -0300

    Mapa-Regiao

--- APS - 3°Semestre/script.js
+++ APS - 3°Semestre/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const svgDoc = mapa.contentDocument;
 
     // Seleciona todos os estados pelo ID ou classe
-    const estados = svgDoc.querySelectorAll('[id^="estado-"]');
+    const estados = svgDoc.querySelectorAll('[id^="estado-BR"]');
 
     estados.forEach((estado) => {
       // Salva a cor original do estado
@@ -17,14 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
         alert(`Você clicou no estado: ${estado.id}`);
       });
 
-      // Adiciona efeito de hover
-      estado.addEventListener("mouseover", () => {
-        estado.style.fill = "#77777780";
+        // Adiciona efeito de hover
+        estado.addEventListener("mouseover", () => {
+          t_regiao = estado.className.animVal.split(" ")[1]
+          
+          regioes = svgDoc.querySelectorAll(`.${t_regiao}`)
+          console.log(regioes)
+          for (let i = 0; i < regioes.length; i++){
+              regioes[i].style.fill = "#1f3b60"
+          }
       });
+
 
       // Retorna à cor original no mouseout
       estado.addEventListener("mouseout", () => {
-        estado.style.fill = corOriginal;
+          t_regiao = estado.className.animVal.split(" ")[1]
+          
+          regioes = svgDoc.querySelectorAll(`.${t_regiao}`)
+          console.log(regioes)
+          for (let i = 0; i < regioes.length; i++){
+              regioes[i].style.fill = corOriginal
+          }
       });
     });
   });
